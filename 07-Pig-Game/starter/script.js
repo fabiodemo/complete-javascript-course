02305efe04dd1707fd93a27e90f3a8b7e5df2b5b
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2021-10-31 11:26:53 -0300

    Section7 Dice rolling and changing of image

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// Selecting Elements:
+//dot is only for selecting classes, when selecting by id, we can do it using #
+const score0El = document.querySelector('#score--0');
+//getElementById is supposed to be a little faster than querySelector
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to currenct score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Switch to Next player
+  }
+});
