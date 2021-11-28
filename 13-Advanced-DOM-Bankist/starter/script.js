57b353d158ded72e46d208fb1c44a69ccc3fee04
fabiodemo <faberdemo@gmail.com>
2021-11-28 10:41:05 -0300

    Half of Section 13

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,147 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// DOM tree is generated from an HTML document, which we can interact with
+// DOM is a very complex API that contains lots of methods and properties to interact with the DOM tree
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+// When you delete from the nodelist using query selector, it will remain saved in the variable
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+// When you delete from the getelements, it will be deleted from the list of allButtons
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
+
+/** Styles, Attributes and Classes */
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+// To get computed styles (real styles that appear on the page or are setted on css)
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+console.log(getComputedStyle(message).height);
+
+// documentElement is the root
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+// set attribute
+logo.alt = 'Beautiful minimalist logo';
+// Attributes (non-standard)
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+// absolute url
+console.log(logo.src);
+// relative url
+console.log(logo.getAttribute('src'));
+
+// const link = document.querySelector('.twitter-link');
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes - we need to convert to cammelcase, even using "-" in html doc
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+// Don't use this, it will overwrite all classes of logo
+logo.className = 'Fabio';
+
+/** Implementing Smooth Scrolling */
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('current scroll (x/y): ', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'height/width viewport: ',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(s1coords.left, s1coords.top); // If in the middle of the first page, only scrolls half of the screen1
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   left: s1coords.top + s1coords.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // Use this to scroll in modern browser/javascript
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+/** Types of Events and Events Handlers */
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+
+  h1.removeEventListener('mouseenter', alertH1);
+};
+
+// Mouse enter is like hover in js
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
+
+// Remove event listener
+
+/** Event Propagation: Bubbling and Capturing */
